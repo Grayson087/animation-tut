1ac6b08a427f2672ec1b46a47659eddf615e0a87
lizrd <glindsay@outlook.com>
2025-12-08 17:14:37 -0500

    done

--- animation tut/Game1.cs
+++ animation tut/Game1.cs
@@ -10,10 +10,11 @@ namespace animation_tut
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        Texture2D greytexture, milkytexture;
-        Rectangle greyrect, milkyrect;
-        Vector2 greyspeed, milkyspeed;
-        int milk;
+        Texture2D greytexture, milkytexture, browntexture,orangetexture;
+        Rectangle greyrect, milkyrect, brownrect, orangerect;
+        Vector2 greyspeed, milkyspeed, brownspeed, orangespeed;
+        int milk, hori, vert ;
+        
         Random generator = new Random();
         int back;
         
@@ -34,12 +35,18 @@ namespace animation_tut
             base.Initialize();
             greyrect = new Rectangle(300, 10, 100, 100);
             milkyrect = new Rectangle(100, 300, 100, 100);
+            brownrect = new Rectangle(100, 300,100, 100);
+            orangerect = new Rectangle(100, 400, 100, 100);
             _graphics.PreferredBackBufferWidth = 800;
             _graphics.PreferredBackBufferHeight = 600;
             _graphics.ApplyChanges();
             greyspeed = new Vector2(2, 2);
             milkyspeed = new Vector2(2, 0);
+            brownspeed = new Vector2(0, 2);
+            orangespeed = new Vector2(4,1);
             back = generator.Next(10);
+            hori = 100;
+            vert = 100;
             
         }
 
@@ -50,6 +57,8 @@ namespace animation_tut
             // TODO: use this.Content to load your game content here
             greytexture = Content.Load<Texture2D>("grey");
             milkytexture = Content.Load<Texture2D>("Milky");
+            browntexture = Content.Load<Texture2D>("brown");
+            orangetexture = Content.Load<Texture2D>("orange");
         }
 
         protected override void Update(GameTime gameTime)
@@ -61,13 +70,27 @@ namespace animation_tut
             greyrect.Y += (int)greyspeed.Y;
             milkyrect.X += (int)milkyspeed.X;
             milkyrect.Y += (int)milkyspeed.Y;
+            brownrect.X += (int)brownspeed.X;
+            brownrect.Y += (int)brownspeed.Y;
+            orangerect.X += (int)orangespeed.X;
+            orangerect.Y += (int)orangespeed.Y;
             if (greyrect.Right > 800 || greyrect.Left < 0)
                 greyspeed.X *= -1;
             if (greyrect.Bottom > _graphics.PreferredBackBufferHeight || greyrect.Top < 0)
                 greyspeed.Y *= -1;
             if (milkyrect.Right > 800 || milkyrect.Left < 0)
                 milkyspeed.X *= -1;
-           
+            if (brownrect.Bottom > 700)
+                brownrect = new Rectangle(100, -100, 100, 100);
+            if (orangerect.Right > 800 || orangerect.Left < 0)
+                orangespeed.X *= -1;
+            if (orangerect.Bottom > _graphics.PreferredBackBufferHeight || orangerect.Top < 0)
+                orangespeed.Y *= -1;
+            
+            
+                
+
+
 
 
 
@@ -90,11 +113,20 @@ namespace animation_tut
             GraphicsDevice.Clear(Color.CornflowerBlue);
             if (milkyrect.Right > 800 || milkyrect.Left < 0)
                 GraphicsDevice.Clear(Color.Green);
-            
+            if (orangerect.Bottom > _graphics.PreferredBackBufferHeight || orangerect.Top < 0)
+                Console.Beep(800, 50);
+            if (orangerect.Right > 800 || orangerect.Left < 0)
+                Console.Beep(800, 50);
+
+
+
+
 
 
             _spriteBatch.Draw(greytexture, greyrect, Color.White);
             _spriteBatch.Draw(milkytexture, milkyrect, Color.White);
+            _spriteBatch.Draw(browntexture, brownrect, Color.White);
+            _spriteBatch.Draw(orangetexture, orangerect, Color.White);
 
 
             _spriteBatch.End();
